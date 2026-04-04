610cafc7c5fb259c3280b0277d5dc1da2aec7265
Lucas-Mac <znac640@live.rhul.ac.uk>
2026-04-04 21:59:07 +0100

    made basic bigram that takes in characters as tokens and also has a generate feature

--- src/bigram.py
+++ src/bigram.py
@@ -1,11 +1,67 @@
 import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from time import sleep
 
 
-class bigram(nn.Module):
-    def __init__(self,vocab_size):
+
+
+class Bigram(nn.Module):
+    def __init__(self,vocab_size,chars):
         super().__init__()
         self.vocab_embedding_table = nn.Embedding(vocab_size,vocab_size)
+        self.encoding_paring ={}
+        self.decoding_paring ={}
+        self.temprature =1.2
 
-    def forward(self,x):
-        logits = self.vocab_embedding_table(x) # raw data not normalised
+        for encoding,char in enumerate(chars):
+            self.encoding_paring[char] = encoding
+            self.decoding_paring[encoding] = char
+
+    def forward(self,idx):
+        logits = self.vocab_embedding_table(idx) # raw data not normalised
         return logits
+    
+   
+    
+    def encode(self,string):
+        values =[]
+        for char in string:
+            values.append(self.encoding_paring.get(char))
+        return values
+
+
+    def decode(self,encodings):
+        decodings =[]
+        for value in encodings:
+            decodings.append(self.decoding_paring.get(value))
+        return decodings
+    
+    def generate(self):
+        
+        idx = torch.zeros((1,1),dtype=torch.long,device='cpu')
+        
+
+        while True:
+            
+            
+            
+            logits = self(idx)
+            logits = logits[:, -1, :]/self.temprature
+            probs = F.softmax(logits,dim =-1)
+            
+            index = torch.multinomial(probs, num_samples=1)
+            
+            
+            output =self.decoding_paring.get(index.item())
+
+            idx = torch.cat((idx,index),dim=1)
+            print(output, end="", flush=True)
+            sleep(0.05)
+            
+            
+        
+        
+        
+    
+        
---
+++ train.py
@@ -0,0 +1,111 @@
+import os
+from dotenv import load_dotenv
+import torch
+import torch.nn.functional as F
+from src.bigram import Bigram
+
+load_dotenv()
+
+
+#device = 'mps' if torch.mps.is_available() else 'cpu'
+device = 'cpu'
+
+
+block_size = 8
+batch_size = 32
+learning_rate = 10e-3
+train_iterations = 1000
+eval_iterations = 500
+
+
+
+
+
+shakespeare_path = os.getenv("SHAKESPEARE_PATH")
+
+
+with open(shakespeare_path,"r") as file:
+    text = file.read() # gets tiny shakespeare text
+chars = sorted(list(set(text))) # get characters in text
+vocab_size = len(chars)
+
+
+#encode and decode chars
+
+
+
+
+def get_batch(data):
+    # Generate random starting points in the text
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    
+    # Stack the chunks into a 2D Tensor
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    
+    # The targets are the same chunks, but shifted right by one
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    
+    return x, y
+# split date into training and val
+
+
+        
+
+
+
+
+
+def main():
+
+
+    model = Bigram(chars=chars,vocab_size=vocab_size) # init model
+    model.to(device)
+
+    data = torch.tensor(model.encode(text), dtype=torch.long)  #encode whole dataset
+
+
+    n = int(len(data)*0.9) # index to split data at 
+
+    train_data =data[:n]
+    val_data =data[n:]  
+
+    train_data.to(device)
+    val_data.to(device)
+
+    optimiser = torch.optim.Adam(model.parameters(),learning_rate)
+
+    for step in range(train_iterations):
+
+        xb,yb = get_batch(train_data) # shape is (B,T) T =8 
+        
+        logits = model(xb) #hsape should be (BATCH_size,T)
+        B, T, C = logits.shape
+        logits = logits.view(B*T,C)
+        targets =yb.view(B*T)
+        loss = F.cross_entropy(logits,targets)
+        #print(loss)
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+    model.generate()
+
+
+        
+
+        
+
+
+
+
+
+
+
+    
+    
+
+main()
+
+
+
+    
